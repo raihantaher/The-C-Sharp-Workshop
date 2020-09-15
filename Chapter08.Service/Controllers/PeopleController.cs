e5e1744842c37836e973c91c5fe94b2c7ba8a714
Raihan Taher <raihan.taher@gmail.com>
2020-09-15 11:29:04 +0800

    Pull from the head repo (#1)
    * Main method is missing
    
    Replaced "Main1" with "Main"
    
    * Fixed chapter1 exercises
    
    * added a comment
    
    * prototyping some examples
    
    * refactoring to static class, started on tests
    
    * extracted GetListInternlAsync, added GetAllStarships
    
    * added a test and StarWars.GetStarshipAsync
    
    * extracted models to new project
    
    * started on chap 8 service project
    
    * progress on local service features
    
    * json files save to folder
    
    * working API get methods
    
    * more tests and refactoring
    
    * implemented swagger
    
    Co-authored-by: JasonHales <JasonHales@users.noreply.github.com>
    Co-authored-by: Mateus Viegas <mateuscviegas@gmail.com>
    Co-authored-by: Adam O'Neil <adamosoftware@gmail.com>

---
+++ Chapter08.Service/Controllers/PeopleController.cs
@@ -0,0 +1,40 @@
+﻿using Chapter08.Models;
+using Chapter08.Service.Static;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Chapter08.Service.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PeopleController : Controller
+    {
+        [HttpGet("{id}")]        
+        public IActionResult Get(int id)
+        {
+            return Ok(JsonFiles.Load<Person>(id));
+        }
+        
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            // to keep it simple, we're not paginating results
+            var data = JsonFiles.LoadAll<Person>();
+
+            return Ok(new Response<List<Person>>()
+            {
+                Count = data.Count(),
+                Data = data.ToList()
+            });
+        }
+
+        [HttpPost]
+        public IActionResult Post(Person person)
+        {
+            JsonFiles.Save(person, person.Url);
+            return Ok();
+        }
+    }
+}
